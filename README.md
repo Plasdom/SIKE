0db267baaed4970cd17c66b128e565c78c56926e
Dominic Power <power8@llnl.gov>
2024-07-22 11:40:19 -0400

    Changes to installation procedure

---
+++ README.md
@@ -0,0 +1,11 @@
+# SIKE
+
+To install, run:
+`
+pip install -e .
+`
+
+Next, install the atomic data:
+`
+python install_atomic_data.py
+`
---
+++ install_atomic_data.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from requests import get
+from zipfile import ZipFile
+import shutil
+
+from sike import constants as c
+
+# TODO: Take install location from input argument, set environment appropriately
+# TODO: Run this script at install
+
+if __name__ == "__main__":
+    # Create directory which will contain atomic data
+    atomic_data_dir = c.ATOMIC_DATA_LOCATION / "atomic_data"
+    atomic_data_dir.mkdir(exist_ok=True)
+
+    # Download and unzip the atomic data for each element
+    for element in c.ELEMENT_LIST:
+        print(f"Downloading atomic data for {element}...")
+        element_zip_name = element + ".zip"
+        element_zip_path = atomic_data_dir / element_zip_name
+        url = c.ATOM_DATA_BASE_URL + element_zip_name + "?download=1"
+        r = get(url=url)
+        open(element_zip_path, "wb").write(r.content)
+        with ZipFile(element_zip_path, "r") as zip_ref:
+            zip_ref.extractall(atomic_data_dir)
+            element_zip_path.unlink()
+            shutil.rmtree(atomic_data_dir / "__MACOSX")
+    print("Finished downloading atomic data.")
