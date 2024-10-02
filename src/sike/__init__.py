004e66e56df37584cff25a24ea000eb65420163f
Dominic Power <power8@llnl.gov>
2024-10-02 18:50:40 -0400

    Added demo notebook

--- src/sike/__init__.py
+++ src/sike/__init__.py
@@ -2,3 +2,4 @@ from sike.core import *
 from sike.analysis.plasma_utils import get_bimaxwellians, get_maxwellians
 from sike.setup import setup
 from sike.analysis import plotting
+from sike.constants import *
